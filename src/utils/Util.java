cb8737a5878cc2f8c4ca907941a3b6dd0ab01b6c
Mialy05 <mialisoamurielle@gmail.com>
2023-03-07 09:12:58 +0300

    [CREATE] FrontServlet, [UPDATE] url mapping for FrontServlet in web.xml

--- src/utils/Util.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-import jakarta.servlet.http.HttpServletRequest;
-
-public class Util {
-    public static String getUrlInfo(HttpServletRequest req) {
-        return req.getPathInfo();
-    }
-}
