4805872ab8f68bc3db4fc19d8a3af84f9dd0ca91
Mialy05 <mialisoamurielle@gmail.com>
2023-03-10 20:54:58 +0300

    [CREATE] etu1834/framework/utils/Util and add function for getting all the classe c:; cd 'c:\Program Files\Apache Software Foundation\Tomcat 10.1\webapps\web-framework'; & 'C:\Program Files\Java\jdk-14.0.1\bin\java.exe' '@C:\Users\Mialisoa\AppData\Local\Temp\cp_cmdgtvd6qtmdcd3cy5lcf6h4e.argfile' 'App'

---
+++ src/app/models/test/Emp.java
@@ -0,0 +1,17 @@
+package app.models.test;
+
+import etu1834.framework.decorator.Url;
+
+public class Emp {
+    int id;
+    String nom;
+
+    public Emp() {
+    }
+
+    @Url(url = "emp-add")
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+
+}
