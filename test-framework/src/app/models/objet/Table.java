10550cdf7175eb72a2862aff6a3034a076a14643
Mialy05 <mialisoamurielle@gmail.com>
2023-06-20 09:21:02 +0300

    [IMPLEMENT] passage de variable de entre FrontServlet et les méthodes dans le modele

--- test-framework/src/app/models/objet/Table.java
+++ test-framework/src/app/models/objet/Table.java
@@ -1,11 +1,19 @@
 package app.models.objet;
 
+import java.util.HashMap;
+
 import etu1834.framework.decorator.Param;
+import etu1834.framework.decorator.Session;
 import etu1834.framework.decorator.Url;
 import etu1834.framework.view.ModelView;
 
 public class Table {
     int count; 
+    HashMap<String, Object> session; 
+
+    public Table() {
+        this.session = new HashMap<String, Object>();
+    }
 
     @Url(url = "table-show.fwk")
     public ModelView show() {
@@ -31,11 +39,42 @@ public class Table {
         return view;
     }
 
+    @Session
+    @Url(url = "table-session.fwk")
+    public ModelView ajouter(@Param(name = "name") String name) {
+        ModelView view = new ModelView();
+
+        this.session.put("article", name);
+
+        view.setView("session");
+        view.addItem("name", "article");
+        view.addItem("value", name);
+        return view;
+    }
+
+    @Session
+    @Url(url = "table-liste.fwk")
+    public ModelView liste() {
+        ModelView view = new ModelView();
+
+        view.setView("liste-table");
+        view.addItem("article", this.session.get("article"));
+        return view;
+    }
+
     public int getCount() {
         return count;
     }
 
     public void setCount(int count) {
         this.count = count;
+    }
+
+    public HashMap<String, Object> getSession() {
+        return session;
+    }
+
+    public void setSession(HashMap<String, Object> session) {
+        this.session = session;
     }
 }
---
+++ test-framework/views/session.jsp
@@ -0,0 +1,1 @@
+Vous avez ajouté une variable de session <%= request.getAttribute("name") %> avec la valeur <%= request.getAttribute("value") %>
